d12dd4874863e870f09af8083be5278ec9346851
Barry Williams <barrywilliams84@gmail.com>
2018-02-22 23:00:04 -0800

    server.js 90% complete, minor modifications to controller and model files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,43 @@
 var express = require("express");
 var burger = require("../models/burger.js");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 // this will query ALL burgers in db
 // burger.selectAll();
 
 
 // Create the `router` for the app, and export the `router` at the end of your file.
+var app = express();
+var port = 3030;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// for INSERT
+app.post("/new-burger", function(req, res) {
+	console.log("INSERT: ", res );
+	var newBurg = res;
+	burger.insertOne(newBurg);
+})
+// for UPDATE
+app.put("/update-burger", function(req, res) {
+	console.log("UPDATE: ", res );
+	var updateBurg = res;
+	burger.updateOne(updateBurg);
+
+})
+// for SELECT
+app.get("/burgers", function(req, res) {
+	console.log("SELECT: ", res );
+	burger.selectAll();
+})
+
+
+
+app.listen(port, function() {
+  console.log("listening on port", port);
+});
--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,15 @@ var orm = require("../config/orm.js");
 
 // * create the code that will call the ORM functions using burger specific input for the ORM.
 
-// * Export at the end of the `burger.js` file.
-
-
 
 var burger = {
 	selectAll: function() {
 		orm.selectAll();
 	},
-	insertOne: function() {
+	insertOne: function(newBurg) {
 		orm.insertOne();
 	},
-	updateOne: function() {
+	updateOne: function(updateBurg) {
 		orm.updateOne();
 	}
 };
